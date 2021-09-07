8cc0c9d31418f912f4c00fee8c9143bf41147a2c
Lucas <lucas.acdc92@gmail.com>
2021-09-07 18:00:53 -0300

    SQL query changed to get the last 10 added

--- backend/routes.js
+++ backend/routes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const routes = express.Router() 
 
+
 routes.get('/', (req, res) => {
     req.getConnection((err, conn) =>{
         if(err) return res.send(err)
 
-        conn.query('SELECT * FROM budgets', (err, rows) => {
+        conn.query('SELECT * FROM budgets ORDER BY date DESC LIMIT 10', (err, rows) => {
             if(err) return res.send(err)
 
             res.json(rows)
